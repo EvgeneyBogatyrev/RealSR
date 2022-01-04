676263302db52ec0ee84d8f9fdf92b34ad928d2a
Evgeney Zimin <evgeneyz@vg-gpu-01.lab.graphicon.ru>
2022-01-04 20:25:03 +0300

    Change interface

--- run.py
+++ run.py
@@ -6,13 +6,12 @@ os.system("chmod -R 0777 /model")
 with open('/model/run.sh', 'w') as f:
 
     videos = os.listdir('/dataset')
-    f.write('mkdir /model/result\n')
 
     for video in videos:
-        f.write(f'mkdir /model/result/{video}\n')
-        f.write(f'python3 /model/codes/test.py -opt /model/codes/options/df2k/test_df2k.yml -in_path /dataset/{video} -res_path /model/result/{video}\n')
+        f.write(f'mkdir /results/{video}\n')
+        f.write(f'python3 /model/codes/test.py -opt /model/codes/options/df2k/test_df2k.yml -in_path /dataset/{video} -res_path /results/{video}\n')
 
-    f.write('chmod -R 0777 /model/result\n')
+    f.write('chmod -R 0777 /results\n')
 
 os.system("chmod -R 0777 /model")
 os.system('/model/run.sh')
